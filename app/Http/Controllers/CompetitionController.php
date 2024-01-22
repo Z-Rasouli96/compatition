62a1f81385b8cc20389d1185b8f8ad90bcf625f8
Z-Rasouli96 <zahra.rasouli2022@gmail.com>
2024-01-22 13:33:07 +0330

    work on save student and cours on controller

--- app/Http/Controllers/CompetitionController.php
+++ app/Http/Controllers/CompetitionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Competition;
 use App\Models\Course;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class CompetitionController extends Controller
@@ -17,6 +18,15 @@ class CompetitionController extends Controller
 
     public function store(Request $request){
 
+        dd($request->competitions);
+
+        $student = Student::create([
+            'username'=> $request->input('username'),
+            'email'=> $request->input('email'),
+
+        ]);
+
+        $student->courses()->sync($request->course_id);
         dd($request->input('username'));
         //  return view('compatition-form');
     }
